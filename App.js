dcafc3b0bebd76d039122875040497aafcb1994e
Vishal Patil <vishalpatil6924@gmail.com>
2023-10-05 22:20:57 +0530

    Frontend changes

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <>
       <SafeAreaWrapper>
         {/* <HomePage/>
-        {/* <LawyerHomePage/> */} */}
+        {/* <LawyerHomePage/> */} 
         {/* <Home/> */}
         <FindLawyer/>
       </SafeAreaWrapper>
